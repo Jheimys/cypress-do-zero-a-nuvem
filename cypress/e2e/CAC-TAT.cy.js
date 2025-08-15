5df17cd9b529b8a622a1032255a9eba08b98d625
Jheimys Bassani <jheimysbb@gmail.com>
2025-08-15 14:39:05 +0100

    Finalizando os primeiros testes

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -17,5 +17,56 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.success strong').should('be.visible').and('contain', 'Mensagem enviada com sucesso.')
 
   })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('ePpM@example')
+    cy.get('#open-text-area').type('mensagem de teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error strong').should('be.visible').and('contain', 'Valide os campos obrigatórios!')
+  })
+
+  it('Campo telefone não aceita letras', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('ePpM@example.com')
+    cy.get('#phone').type('abcdefghij')
+    cy.get('#open-text-area').type('mensagem de teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.success strong').should('be.visible').and('contain', 'Mensagem enviada com sucesso.')
+  })
+
+   it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('ePpM@example.com')
+    cy.get('#phone-checkbox').click()  
+    cy.get('#open-text-area').type('mensagem de teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error strong').should('be.visible').and('contain', 'Valide os campos obrigatórios!')
+  })
+
+   it('prepreenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName').type('João').should('have.value', 'João').clear().should('have.value', '')
+    cy.get('#lastName').type('Silva').should('have.value', 'Silva').clear().should('have.value', '')
+    cy.get('#email').type('ePpM@example.com').should('have.value', 'ePpM@example.com').clear().should('have.value', '')   
+  
+  })
+
+   it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    
+     cy.get('button[type="submit"]').click()
+
+    cy.get('.error strong').should('be.visible').and('contain', 'Valide os campos obrigatórios!')
+  
+  })  
+
+  it.only('envia o formuário com sucesso usando um comando customizado', () => {
+    cy.fillMandatoryFieldsAndSubmit('João', 'Silva', 'ePpM@example.com', 'mensagem de teste')    
+  })
   
 })
