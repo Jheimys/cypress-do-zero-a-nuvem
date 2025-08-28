8ac9aa2a2d29b96ed04293c72b92d714455f8645
Jheimys Bassani <jheimysbb@gmail.com>
2025-08-28 19:23:57 +0100

    Titulo teste alterado

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -147,7 +147,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.contains('h1','CAC TAT - Política de Privacidade').should('be.visible')
   })
 
-  it('testa a página da política de privacidade de forma independente', () => {
+  it('testa a página da política de privacidade de forma independente.', () => {
     cy.visit('./src/privacy.html')
     cy.contains('CAC TAT - Política de Privacidade').should('be.visible')
   })
